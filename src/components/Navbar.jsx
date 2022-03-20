0d51210358668498ce1785173c24cdf7df6c2679
black ninja <iamathis@ninja.com>
2022-03-20 13:22:42 +0000

    Add Styling for navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,23 +3,26 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Logo = styled(Typography)`
-font-size:2.3rem;
-font-family:Helvetica;
-`
+  font-size: 2.3rem;
+  font-family: Helvetica;
+  color: white;
+  span{
+    background:red;
+    padding:0px 6.5px;
+    border-radius:6.5px;
+  }
+`;
 
 function Navbar() {
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+        <AppBar position="fixed" color="transparent" elevation={0}>
           <Toolbar>
-              <Logo>swisscalc</Logo>
+            <Logo> swisscalc <span>+</span> </Logo>
           </Toolbar>
         </AppBar>
       </Box>
