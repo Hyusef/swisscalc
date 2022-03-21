3630362655806a3175c36d3654fd92da2f748762
therealestdeal <mieyle@protonmail.com>
2022-03-21 12:00:44 +0000

    Add styling for background and header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,59 @@
 import React from "react";
 import Navbar from "./Navbar";
 import styled from "styled-components";
-import backgroundImage from "../Assets/Images/backgroundSwiss.svg";
 
 const MainHeaderContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: url(${backgroundImage});
-  background-position: center;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  background-size: cover;
-  -o-background-size: cover;
+  display: flex;
+  justify-content: center;
+  background: linear-gradient(90deg, #e74d4d, #8c000c);
+  background-size: 200% 100%;
+  animation: gradient 30s alternate infinite;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 80vh;
+  width: 100%;
+
+  @keyframes gradient {
+    0% {
+      background-position: 0%;
+    }
+    100% {
+      background-position: 100%;
+    }
+  }
+`;
+
+const Imgu = styled.img`
+  width: 70%;
+  margin: 15%;
+`;
+
+const Headers = styled.h1`
+  font-size: 4rem;
+  color: white;
+  font-family: "Source Serif Pro", serif;
+`;
+
+const HeadersContainer = styled.div`
+  margin-top: 10%;
+  p {
+    color: #ffffff;
+    font-size: 1.8rem;
+    font-weight: 300;
+    font-family: "Source Serif Pro", serif;
+    text-align: center;
+  }
 `;
 
 function Header() {
   return (
     <MainHeaderContainer>
       <Navbar />
+      <HeadersContainer>
+        <Headers>Multi-Purpose Swiss Calculator</Headers>
+        <p>Just like the Swiss knife, the Swiss calc is a powerful tool.</p>
+      </HeadersContainer>
     </MainHeaderContainer>
   );
 }
