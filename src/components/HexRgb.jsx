c3e98ac2ec03f76b56873d3825d0bc4067d2ec9b
therealestdeal <mieyle@protonmail.com>
2022-03-22 13:09:49 +0000

    Add styling for coontainers

--- src/components/HexRgb.jsx
+++ src/components/HexRgb.jsx
@@ -5,6 +5,24 @@ import Chip from "@mui/material/Chip";
 import { useState } from "react";
 import swal from "sweetalert";
 
+const CalcContainer = styled(Paper)`
+  margin: 20px;
+  width: 300px;
+  text-align: center;
+  dispay: flex;
+  flex-direction: row;
+  border: 1px solid pink;
+  .chip {
+  }
+
+  .chipContainer {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0px;
+  }
+`;
+
+
 function HexRgb() {
     return (
         <div>
