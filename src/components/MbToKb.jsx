48b8a9368e6fb5e6778df522ed57e133b8b1ce89
therealestdeal <mieyle@protonmail.com>
2022-03-23 22:46:05 +0000

    Add all handlers and states to component

--- src/components/MbToKb.jsx
+++ src/components/MbToKb.jsx
@@ -15,13 +15,60 @@ const CalcContainer = styled(Paper)`
 
   .chipContainer {
     display: flex;
-    justify-content: space-around;
+    justify-cntent: space-around;
     margin: 15px 0px;
   }
 `;
 
 function MbToKb() {
-  return <div></div>;
+  const [outPut, setOutPut] = useState("");
+  const [input, setInput] = useState("");
+
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
+
+  const mbToKbHandler = () => {
+    if (input === "" || input < 0) {
+      swal("Error", "Please Enter a valid value", "error");
+      return;
+    }
+    setOutPut((+input * 1000).toFixed(2) + " Kb");
+  };
+
+  const kbToMbHandler = () => {
+    if (input === "" || input < 0) {
+      swal("Error", "Please Enter a valid value", "error");
+      return;
+    }
+    setOutPut(+input / 1000 + " Mb");
+  };
+  return (
+    <CalcContainer>
+      <h2>Megabytes 🔄 Kilobytes </h2>    
+      <div className="chipContainer">
+        <Chip
+          className="chip"
+          variant="outlined"
+          label="Megabytes To Kilobytes"
+          onClick={mbToKbHandler}
+        />
+        <Chip 
+          className="chip"
+          variant="outlined"
+          label="Kilobytes To Megabytes"
+          onClick={kbToMbHandler}
+        />
+      </div>
+      <input
+        required
+        name="number"
+        type="number"
+        onChange={inputHandler}
+      ></input>
+      <p>{outPut}</p>
+    </CalcContainer>
+  );
 }
 
 export default MbToKb;
