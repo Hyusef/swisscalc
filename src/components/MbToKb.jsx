884d3f17cc2eb6339b26fefb1b283e53d89ed7fb
therealestdeal <mieyle@protonmail.com>
2022-03-23 22:34:22 +0000

    Add styling for container which will become component l8r

--- src/components/MbToKb.jsx
+++ src/components/MbToKb.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
 import Paper from "@mui/material/Paper";
 import styled from "styled-components";
 import Chip from "@mui/material/Chip";
 import { useState } from "react";
 import swal from "sweetalert";
 
+const CalcContainer = styled(Paper)`
+  margin: 20px;
+  width: 350px;
+  text-align: center;
+  dispay: flex;
+  flex-direction: row;
+  border: 1px solid pink;
+
+  .chipContainer {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0px;
+  }
+`;
+
 function MbToKb() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return <div></div>;
 }
 
-export default MbToKb
+export default MbToKb;
