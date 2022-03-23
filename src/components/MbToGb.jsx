00be86b1c270e913030d60b4ab79c5acbe4d360f
therealestdeal <mieyle@protonmail.com>
2022-03-23 21:59:24 +0000

    Add styling for container

--- src/components/MbToGb.jsx
+++ src/components/MbToGb.jsx
@@ -5,6 +5,21 @@ import Chip from "@mui/material/Chip";
 import { useState } from "react";
 import swal from "sweetalert";
 
+const CalcContainer = styled(Paper)`
+  margin: 20px;
+  width: 300px;
+  text-align: center;
+  dispay: flex;
+  flex-direction: row;
+  border: 1px solid pink;
+
+  .chipContainer {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0px;
+  }
+`;
+
 
 function MbToGb() {
     return (
